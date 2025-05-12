bc25493fd7db8df3902bcb2efcbcd9688ac1c8fc
Divyanshu T <tdivyanshu028@gmail.com>
2025-05-12 03:28:07 -0700

    Updated

--- lib/containmain.dart
+++ lib/containmain.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'header.dart';
-import 'slider_drawer.dart'; // CustomDrawer is defined here
+import 'slider_drawer.dart';
 import 'home1.dart';
 import 'home2.dart';
 import 'home3.dart';
@@ -13,6 +13,7 @@ import 'home9.dart';
 import 'home10.dart';
 import 'home11.dart';
 import 'footer.dart';
+
 class ContainMain extends StatelessWidget {
   const ContainMain({super.key});
 
@@ -20,25 +21,25 @@ class ContainMain extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       endDrawer: const CustomDrawer(),
-      body: SingleChildScrollView(
-        child: Column(
-          children: const [
-            Header(),
-            
-            Demo1(),
-            ServiceCardsSection(),
-            HomeSection(),
-            Home4(),
-            //Home5(),
-            //Home6(),
-            Home7(),
-            Home8(),
-            Home9(),
-            Home10(),
-            Home11(),
-            Footer(),
-            // Other sections...
-          ],
+      body: SafeArea( // WRAPPED ENTIRE BODY TO AVOID OVERFLOW
+        child: SingleChildScrollView(
+          child: Column(
+            children: const [
+              Header(),
+              Demo1(),
+              ServiceCardsSection(),
+              Home3(),
+              Home4(),
+              Home5(),
+              Home6(),
+              Home7(),
+              Home8(),
+              Home9(),
+              Home10(),
+              Home11(),
+              Footer(),
+            ],
+          ),
         ),
       ),
     );
