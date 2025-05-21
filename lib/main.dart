6f8557a5665df11b9fd4007980876f915a3a7b0b
Pooja Devrari <165161097+poojaDEvrari@users.noreply.github.com>
2025-05-21 12:39:29 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+Main :
+
 import 'package:flutter/material.dart';
 import 'containmain.dart';
-
+import 'Service/containcloud.dart';
+import 'Web_Service/containweb.dart';
+import 'AI_Service/containai.dart';
+// import 'path_to/DigitalMarketingPage.dart'; // Uncomment and correct as needed
+// Uncomment and correct as needed
+import 'DM_Service/containdm.dart';
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
@@ -8,9 +15,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ContainMain(),
+      home: const ContainMain(),
+      routes: {
+        '/cloud-services': (context) => ContainCloud(),
+        '/development-services': (context) => ContainWeb(),
+        '/ai-ml-services': (context) => ContainAI(),
+        '/digital-marketing': (context) => ContainDM(), // Replace with your widget
+         // Replace with your widget
+      },
     );
   }
 }
